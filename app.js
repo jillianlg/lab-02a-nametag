cf7e82161fd9539d335bae0a62008879da552bd4
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-02 16:50:48 -0700

    add .js, user intput and btn

---
+++ app.js
@@ -0,0 +1,10 @@
+const myName = document.getElementById('name');
+const myInput = document.getElementById('name-input');
+const myButton = document.getElementById('name-button');
+
+myButton.addEventListener('click', () => {
+
+    const userInput = myInput.value;
+
+    myName.textContent = userInput;
+});
